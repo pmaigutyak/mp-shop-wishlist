8c379ae501eb6602887b4a1700b8aa2f963c360b
Pasha Maigutyak <pmaigutyak@gmail.com>
2022-09-19 17:40:09 +0300

    Fix issue.

--- wishlist/__init__.py
+++ wishlist/__init__.py
@@ -3,6 +3,11 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
+def setup_settings(settings, is_prod, **kwargs):
+
+    settings['MIDDLEWARE'] += ['wishlist.middleware.WishListMiddleware']
+
+
 class WishListAppConfig(AppConfig):
 
     name = 'wishlist'
